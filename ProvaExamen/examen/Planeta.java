9225180e6063b75482ed61c01acc54853d7b3840
24Victor <borenvictorzheng@iesmontsia.org>
2023-02-27 09:00:42 +0100

    t

--- ProvaExamen/examen/Planeta.java
+++ ProvaExamen/examen/Planeta.java
@@ -1,6 +1,8 @@
 public class Planeta 
 {
     //DOCUMENTAR
+    /*Assignem variables, variable nom amb tipus de dada String, 
+    diametre amb tipus int i variable pes amb tipus enter*/
     String nom;
     int diametre;
     int pes;    
@@ -9,18 +11,22 @@ public class Planeta
     //Contructor amb tots els parametres    
     public Planeta(String nom, int diametre, int pes)
     {
+        //assignem el valor de nom del constructor planeta al nom del class Planeta
         this.nom = nom;
+        //assignem el valor de diametre del constructor planeta al nom del class diametre
         this.diametre = diametre;
+        //assignem el valor de pes del constructor planeta al pes del class Planeta
         this.pes = pes;
     }
 
     //DOCUMENTAR
-    //Contructor buit
+    //Estem creant un public Contructor Planeta què està buit
     public Planeta(){}
 
     //DOCUMENTAR
     public int planetaEnano()
     {
+        //Si diametre es menor que 1300 retorna 1 i sino retorna 0.
         if(this.diametre < 1300)
         {
             return 1;
@@ -34,15 +40,22 @@ public class Planeta
     //DOCUMENTAR
     public void expansio()
     {        
-        //ACABAR
+        //Fem que el diamtre es multiple * 3 als planetes, i com es void no cal fer un return.
+        this.diametre = diametre * 3;
     }
 
     //DOCUMENTAR
     public int colisio(int meteorito)
     {
-        //ACABAR
-        return -1;
-       
+        //Si el pes es mes gran o igual a 1800 retorna 1
+        if(this.pes>= 1800){
+            return 1;
+        }
+        //sino el diamtre se suma la mitad del pes del meteorito i retona 0
+        else{
+            this.diametre = diametre + meteorito/2;
+            return 0;
+        }
     }
 
     //Getters i setters
