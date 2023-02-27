e4121c6bb5f304a480b3d6488261cb7a1eb9b6fc
24Victor <borenvictorzheng@iesmontsia.org>
2023-02-27 09:40:31 +0100

    G

--- ProvaExamen/examen/PlanetaTest.java
+++ ProvaExamen/examen/PlanetaTest.java
@@ -1,26 +1,49 @@
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
+/** 
+ * @author Victor Zheng
+ * @version 1.0
+ * @since 30-01-2023
+ */
 
 public class PlanetaTest {    
 
+    /**
+     * @param
+     */
     @Test 
     public void testPlanetaEnano()
     {     
+       String nom = "PlanetaEnano";
+       int diametre = 1200;
+       int pes = 1800;
+       
+       Planeta planeta = new Planeta(nom, diametre, pes);
+ 
 
-        //assertEquals(expected, actual);
-        //assertEquals(expected, actual);
-
-    }    
-
+        assertEquals(planeta.getPlanetaEnano());
+    }
+     
+    /**
+     * @param
+     */
     @Test 
     public void testExpansio()
     {
+        int diametre1 = 1800;
+        Planeta planeta = new Planeta(nom, diametre);
 
+        float expected = 1800 * 3;
+        float actual = planeta.getExpansio(diametre);
+        assertEquals(expected, actual);
         //assertEquals(expected, actual);
 
     }
 
+    /**
+     * @param
+     */
     @Test 
     public void testColisio()
     {        
